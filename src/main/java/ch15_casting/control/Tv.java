11d8b02e4c53f3e1b78a92cf90c6ea3423e60874
yunggeun0627 <bbiyagi465@gmail.com>
2025-05-14 12:41:44 +0900

    Update Tv.java

--- src/main/java/ch15_casting/control/Tv.java
+++ src/main/java/ch15_casting/control/Tv.java
@@ -8,7 +8,7 @@ public class Tv implements Power{
 
     @Override
     public void off() {
-        System.out.println("스피코을 끕니다.");
+        System.out.println("스피커을 끕니다.");
     }
     public void channelUp() {
         System.out.println("채널을 한 칸 올립니다.");
